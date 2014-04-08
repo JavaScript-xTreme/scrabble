76b6042991ca8ef723caa86bd7c2996b5ceeb2d4
Chyld Medford <chyld.medford@gmail.com>
2014-04-07 23:25:12 -0500

    part 1

--- app/lib/sockets.js
+++ app/lib/sockets.js
@@ -1,5 +1,45 @@
 'use strict';
 
+global.scrabble = {};
+global.scrabble.users = {};
+
 exports.connection = function(socket){
   socket.emit('online', {date: new Date()});
+  socket.on('register', register);
+  socket.on('start', start);
+  socket.on('letters', letters);
 };
+
+function register(data){
+  var socket = this;
+  global.scrabble.users[socket.id] = {name: data.name};
+  socket.broadcast.emit('joined', data);
+}
+
+function start(){
+  var socket = this;
+  socket.emit('begin');
+  socket.broadcast.emit('begin');
+}
+
+function letters(){
+    var socket = this;
+
+    if(!global.scrabble.users[socket.id].letters || global.scrabble.users[socket.id].letters.length === 0){
+      var letters = getString();
+      global.scrabble.users[socket.id].letters = letters;
+      socket.emit('roll', {letters:letters});
+    }
+}
+
+function getString(){
+  var strings = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  var string = '';
+
+  for(var i = 0; i < 10; i++){
+    var index = Math.floor(Math.random() * 26);
+    string += strings[index];
+  }
+
+  return string;
+}
--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -8,11 +8,46 @@
 
   function initialize(){
     initializeSocketIO();
+    $('#reg-btn').click(register);
+    $('#start').click(start);
+    $('#letters').click(letters);
   }
 
  function initializeSocketIO(){
     socket = io.connect('/game');
     socket.on('online', function(data){console.log(data);});
+    socket.on('joined', joined);
+    socket.on('begin', begin);
+    socket.on('roll', roll);
+  }
+
+  function roll(data){
+    for(var i = 0; i < data.letters.length; i++){
+      $('#pieces').append('<li>'+data.letters[i]+'</li>');
+    }
+  }
+
+  function register(){
+    var name = $('#reg').val();
+    $('#user').text(name);
+    $('#reg').val('');
+    socket.emit('register', {name:name});
+  }
+
+  function joined(data){
+    $('#users').append('<li>'+data.name+'</li>');
+  }
+
+  function start(){
+    socket.emit('start');
+  };
+
+  function begin(){
+    $('#start').detach();
+  }
+
+  function letters(){
+    socket.emit('letters');
   }
 
 })();
