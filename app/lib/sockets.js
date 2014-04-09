2665a3406cfa3a3ab52db9651b264774652161a4
Chyld Medford <chyld.medford@gmail.com>
2014-04-08 21:09:34 -0500

    class 1

--- app/lib/sockets.js
+++ app/lib/sockets.js
@@ -2,4 +2,26 @@
 
 exports.connection = function(socket){
   socket.emit('online', {date: new Date()});
+  socket.on('join', join);
+  socket.on('getletters', getletters);
 };
+
+function getletters(data){
+  var socket = this;
+
+  var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  letters = letters.split('');
+  var list = [];
+  for(var i = 0; i < data.count; i++){
+    var index = Math.floor(Math.random() * 26);
+    list.push(letters[index]);
+  }
+  socket.broadcast.emit('setletters', {user:data.user, letters:list});
+  socket.emit('setletters', {user:data.user, letters:list});
+}
+
+function join(data){
+  var socket = this;
+
+  socket.broadcast.emit('joined', data);
+}
--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -5,14 +5,46 @@
   $(document).ready(initialize);
 
   var socket;
+  var game = {};
 
   function initialize(){
     initializeSocketIO();
+    $('#join').click(join);
+    $('#pick').click(pick);
+  }
+
+  function setletters(data){
+    console.log('here are the letters');
+    console.log(data);
+  }
+
+  function pick(){
+    var count = $('#count').val() * 1;
+    var total = game.letters.length + count;
+    if(total <= 7){
+      socket.emit('getletters', {user:game.user, count:count});
+    }
+  }
+
+  function join(){
+    var user = $('#user').val();
+    game.user = user;
+    game.letters = [];
+    var tr = '<tr class="primary" data-user="' + user + '"><td class="user">' + user + '</td><td class="letters"></td></tr>';
+    $('#users > tbody').append(tr);
+    socket.emit('join', {user:user});
+  }
+
+  function joined(data){
+    var tr = '<tr class="secondary" data-user="' + data.user + '"><td class="user">' + data.user + '</td><td class="letters"></td></tr>';
+    $('#users > tbody').append(tr);
   }
 
  function initializeSocketIO(){
     socket = io.connect('/game');
     socket.on('online', function(data){console.log(data);});
+    socket.on('joined', joined);
+    socket.on('setletters', setletters);
   }
 
 })();
